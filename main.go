24dbb82728054aa061d14b90a0fcd10e79be0f18
zhs007 <sssxueren@gmail.com>
2018-10-21 15:00:00 +0800

    upd

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"github.com/zhs007/jarvistelebot/chatbot"
 	"github.com/zhs007/jarvistelebot/telebot"
 )
 
 func main() {
 	err := telebot.InitTeleBot("./cfg/config.yaml")
 	if err != nil {
-		telebot.Error("InitTeleBot err.")
+		chatbot.Error("InitTeleBot err.")
 	}
 
 	defer telebot.ReleaseTeleBot()
