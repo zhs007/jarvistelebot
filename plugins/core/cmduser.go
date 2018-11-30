0bdae1f74692a4599f3b5392f6518f0bc92795cb
zhs007 <sssxueren@gmail.com>
2018-11-30 11:59:25 +0800

    update core.user

--- plugins/core/cmduser.go
+++ plugins/core/cmduser.go
@@ -6,6 +6,7 @@ import (
 	"github.com/golang/protobuf/proto"
 	"github.com/spf13/pflag"
 	"github.com/zhs007/jarvistelebot/chatbot"
+	"github.com/zhs007/jarvistelebot/chatbotdb/proto"
 	"github.com/zhs007/jarvistelebot/plugins/core/proto"
 )
 
@@ -15,21 +16,64 @@ type cmdUser struct {
 
 // RunCommand - run command
 func (cmd *cmdUser) RunCommand(ctx context.Context, params *chatbot.MessageParams) bool {
-	if len(params.LstStr) == 3 {
-		lst, err := params.ChatBot.GetChatBotDB().GetUser(params.LstStr[2])
-		if err != nil {
-			chatbot.SendTextMsg(params.ChatBot, params.Msg.GetFrom(), err.Error())
+	if params.CommandLine != nil {
+		usercmd, ok := params.CommandLine.(*plugincorepb.UserCommand)
+		if !ok {
+			return false
 		}
 
-		strret, err := chatbot.FormatJSONObj(lst)
-		if err != nil {
-			chatbot.SendTextMsg(params.ChatBot, params.Msg.GetFrom(), err.Error())
-		} else {
-			chatbot.SendTextMsg(params.ChatBot, params.Msg.GetFrom(), strret)
+		var user *chatbotdbpb.User
+		if usercmd.UserID != "" {
+			userbyuid, err := params.ChatBot.GetChatBotDB().GetUser(usercmd.UserID)
+			if err != nil {
+				chatbot.SendTextMsg(params.ChatBot, params.Msg.GetFrom(), err.Error())
+
+				return false
+			}
+
+			user = userbyuid
+		} else if usercmd.UserName != "" {
+			userbyuid, err := params.ChatBot.GetChatBotDB().GetUserWithUserName(usercmd.UserName)
+			if err != nil {
+				chatbot.SendTextMsg(params.ChatBot, params.Msg.GetFrom(), err.Error())
+
+				return false
+			}
+
+			user = userbyuid
 		}
+
+		if user != nil {
+			strret, err := chatbot.FormatJSONObj(user)
+			if err != nil {
+				chatbot.SendTextMsg(params.ChatBot, params.Msg.GetFrom(), err.Error())
+			} else {
+				chatbot.SendTextMsg(params.ChatBot, params.Msg.GetFrom(), strret)
+			}
+
+			return true
+		}
+
+		chatbot.SendTextMsg(params.ChatBot, params.Msg.GetFrom(), "Sorry, I didn't find this user.")
 
 		return true
 	}
+
+	// if len(params.LstStr) == 3 {
+	// 	lst, err := params.ChatBot.GetChatBotDB().GetUser(params.LstStr[2])
+	// 	if err != nil {
+	// 		chatbot.SendTextMsg(params.ChatBot, params.Msg.GetFrom(), err.Error())
+	// 	}
+
+	// 	strret, err := chatbot.FormatJSONObj(lst)
+	// 	if err != nil {
+	// 		chatbot.SendTextMsg(params.ChatBot, params.Msg.GetFrom(), err.Error())
+	// 	} else {
+	// 		chatbot.SendTextMsg(params.ChatBot, params.Msg.GetFrom(), strret)
+	// 	}
+
+	// 	return true
+	// }
 
 	return false
 }
