8d414d5a90e5dcb540e2759ef8c01fc35bab782f
zhs007 <sssxueren@gmail.com>
2018-12-16 09:31:12 +0800

    updated jarviscore to 0.6.21

--- basedef/basedef.go
+++ basedef/basedef.go
@@ -4,7 +4,7 @@ package basedef
 const JARVISNODETYPE = "telebot.jarvis"
 
 // VERSION - version
-const VERSION = "0.1.100"
+const VERSION = "0.1.101"
 
 // MaxTextMessageSize - max message text size
 const MaxTextMessageSize = 4096
