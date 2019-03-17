e87f28ccedd8de8465e6aa60970a1fd239127385
zhs007 <sssxueren@gmail.com>
2019-03-17 17:11:58 +0800

    - updated jarviscore to v0.7.87

--- basedef/basedef.go
+++ basedef/basedef.go
@@ -4,7 +4,7 @@ package basedef
 const JARVISNODETYPE = "telebot.jarvis"
 
 // VERSION - version
-const VERSION = "0.2.32"
+const VERSION = "0.2.33"
 
 // MaxTextMessageSize - max message text size
 const MaxTextMessageSize = 4096
