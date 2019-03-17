8c9580fbd6becb00bc8b768e1972096b941e6676
zhs007 <sssxueren@gmail.com>
2019-03-17 15:19:39 +0800

    - updated jarviscore to v0.7.85 - when the runscript error, it will reply the output information - fixed #39

--- basedef/basedef.go
+++ basedef/basedef.go
@@ -4,7 +4,7 @@ package basedef
 const JARVISNODETYPE = "telebot.jarvis"
 
 // VERSION - version
-const VERSION = "0.2.31"
+const VERSION = "0.2.32"
 
 // MaxTextMessageSize - max message text size
 const MaxTextMessageSize = 4096
