3febd790959b5ea8f09bdf6d4518b390962e524e
zhs007 <sssxueren@gmail.com>
2019-04-05 20:22:22 +0800

    - added testing

--- plugins/translate/cmdtranslate_test.go
+++ plugins/translate/cmdtranslate_test.go
@@ -27,9 +27,33 @@ func TestParseTranslateCmd(t *testing.T) {
 	}
 
 	for i := 0; i < len(lst); i++ {
-		_, err := parseTranslateCmd(lst[i].lststr)
+		cmd, err := parseTranslateCmd(lst[i].lststr)
 		if err != lst[i].err {
 			t.Fatalf("TestParseTranslateCmd parseTranslateCmd %v - %v", lst[i], err)
+		}
+
+		if lst[i].cmd != nil && cmd == nil || lst[i].cmd == nil && cmd != nil {
+			t.Fatalf("TestParseTranslateCmd parseTranslateCmd %v - %v", lst[i], cmd)
+		} else if cmd != nil && lst[i].cmd != nil {
+			if cmd.Platform != lst[i].cmd.Platform {
+				t.Fatalf("TestParseTranslateCmd parseTranslateCmd %v %v - %v",
+					lst[i].lststr, lst[i].cmd.Platform, cmd.Platform)
+			}
+
+			if cmd.SrcLang != lst[i].cmd.SrcLang {
+				t.Fatalf("TestParseTranslateCmd parseTranslateCmd %v %v - %v",
+					lst[i].lststr, lst[i].cmd.SrcLang, cmd.SrcLang)
+			}
+
+			if cmd.DestLang != lst[i].cmd.DestLang {
+				t.Fatalf("TestParseTranslateCmd parseTranslateCmd %v %v - %v",
+					lst[i].lststr, lst[i].cmd.DestLang, cmd.DestLang)
+			}
+
+			if cmd.Run != lst[i].cmd.Run {
+				t.Fatalf("TestParseTranslateCmd parseTranslateCmd %v %v - %v",
+					lst[i].lststr, lst[i].cmd.Run, cmd.Run)
+			}
 		}
 
 		// if cmd != lst[i].cmd {
