cdeac25214e5efb5bd01a73d8321a55b784e5712
zhs007 <sssxueren@gmail.com>
2019-04-14 17:19:55 +0800

    - updated getarticles

--- basedef/basedef.go
+++ basedef/basedef.go
@@ -4,7 +4,7 @@ package basedef
 const JARVISNODETYPE = "telebot.jarvis"
 
 // VERSION - version
-const VERSION = "0.2.108"
+const VERSION = "0.2.109"
 
 // MaxTextMessageSize - max message text size
 const MaxTextMessageSize = 4096
